52da878ff58c01b1358f4a1e6f79ac1905d61073
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-14 14:54:03 -0300

    vamola2

--- api/Post.js
+++ api/Post.js
@@ -9,8 +9,8 @@ const App = require("../chat")
  */
 router.post("/", async (req, res) => {
   try {
-    //var resposta = await App(req.body.coment)
-    res.status(200).send("bom dia");
+    var resposta = await App(req.body.coment)
+    res.status(200).send(resposta);
 
   } catch (error) {
     return res.status(500).send("Server error");

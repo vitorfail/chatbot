9e1a44202b09aa9385c2e426971308066612f121
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-14 11:45:03 -0300

    Raquel1

--- src/Routes/UsuarioRoute.js
+++ src/Routes/UsuarioRoute.js
@@ -3,6 +3,6 @@ module.exports = (app) => {
    app.post('/chat', UsuarioController.post);
    app.put('/chat/:id', UsuarioController.put);
    app.delete('/chat/:id', UsuarioController.delete);
-   app.get('/chatbot', UsuarioController.get);
+   app.get('/', UsuarioController.get);
    app.get('/chat/:id', UsuarioController.getById);
 }

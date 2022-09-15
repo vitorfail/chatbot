e80bd64b43df7f2a289c75c17e18e6599257aba9
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-15 17:34:07 -0300

    123ed

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const Post = require("./api/Post");
 
 app.use(express.json({ extended: false }));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use("/api/post", Post);
 
 const PORT = process.env.PORT || 8080;
